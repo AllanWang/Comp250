8f2bb247e7db0851db4e42c39b22532d6b18f295
AllanWang <me@allanwang.ca>
2016-10-29 17:48:25 -0400

    foreach

--- src/comp202_2016/midterm/Midterm.java
+++ src/comp202_2016/midterm/Midterm.java
@@ -7,8 +7,8 @@ public class Midterm {
 
     public static int sum(int[] array) {
         int sum = 0; //create integer that will hold the sum
-        for (int i = 0; i < array.length; i++) { //loop through all elements
-            sum += array[i]; //add element to sum
+        for (int number : array) { //loop through all numbers
+            sum += number; //add number to sum
         }
         return sum;
     }
@@ -16,15 +16,15 @@ public class Midterm {
     public static int[] removeNegatives(int[] array) {
         //get the number of non-negative values first so you know the size of the new array
         int newSize = 0;
-        for(int i = 0; i < array.length; i++) {
-            if (array[i] >= 0) newSize++; //increase count by one for every non-negative number
+        for (int number : array) {
+            if (number >= 0) newSize++; //increase count by one for every non-negative number
         }
         //newSize now holds the number of non-negative values; initialize array
         int[] newArray = new int[newSize];
         int index = 0; //you'll need this to keep track of where to add the new element into newArray
-        for (int i = 0; i < array.length; i++) { //loop through ORIGINAL array again
-            if (array[i] >= 0) { //number is non-negative, add to new array
-                newArray[index] = array[i];
+        for (int number : array) { //loop through ORIGINAL array again
+            if (number >= 0) { //number is non-negative, add to new array
+                newArray[index] = number;
                 index++; //next number will now be added to new index
             }
         }
