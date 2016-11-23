591849f79a1966dc1f7b4ddd77817f9208089be1
AllanWang <me@allanwang.ca>
2016-11-23 02:46:57 -0500

    finish tester

--- src/a4q1/HashTableTester2.java
+++ src/a4q1/HashTableTester2.java
@@ -1,145 +1,169 @@
 package a4q1;
 
-import java.util.ArrayList;
+import org.omg.CORBA.OBJ_ADAPTER;
+
+import java.util.*;
 
 
 /**
  * Class for testing your implementation of the HashTable class.
+ * <p>
+ * Slightly modified to better reflect errors that occur - Allan
+ * All tests will be compared to a HashMap to ensure that no errors are made within the tests
  */
 public class HashTableTester2 {
 
-	/**
-	 * Returns a list of songs to use for testing the hash table.
-	 * @return A list of songs to use for testing the hash table
-	 */
-	private static ArrayList<Song> initSongList() {
-		ArrayList<Song> songs = new ArrayList<Song>();
-		songs.add(new Song("Paranoid Android", "Radiohead", 1997));
-		songs.add(new Song("Machine Gun", "Slowdive", 1993));
-		songs.add(new Song("A Change Is Gonna Come", "Sam Cooke", 1964));
-		songs.add(new Song("15 Step", "Radiohead", 2008));
-		songs.add(new Song("Blue Line Swinger", "Yo La Tengo", 1995));
-		songs.add(new Song("Reckoner", "Radiohead", 2008));
-		songs.add(new Song("Ashes To Ashes", "David Bowie", 1980));
-		songs.add(new Song("Speed Law", "Mos Def", 1999));
-		songs.add(new Song("93 'Til Infinity", "Souls of Mischief", 1993));
-		songs.add(new Song("Season of the Shark", "Yo La Tengo", 2003));
-		songs.add(new Song("Cet Air-La", "France Gall", 1966));
-		songs.add(new Song("Space Oddity", "David Bowie", 1969));
-		songs.add(new Song("Il Nous Faut Regarder", "Jacques Brel", 1955));
-		songs.add(new Song("Happy Holidays", "Jim O'Rourke", 1999));
-		songs.add(new Song("Le Premier Bonheur du Jour", "Os Mutantes", 1968));
-		songs.add(new Song("Stretch Out And Wait", "The Smiths", 1987));
-		songs.add(new Song("Scream", "Black Flag", 1984));
-		songs.add(new Song("Europe, After the Rain", "Max Richter", 2002));
-		songs.add(new Song("Why Are You Looking Grave?", "Mew", 2005));
-		songs.add(new Song("Fallen Angel", "King Crimson", 1974));
-		songs.add(new Song("Milk and Honey", "Nick Drake", 2007));
-		songs.add(new Song("One Less Bell To Answer", "Burt Bacharach", 2003));
-		songs.add(new Song("A Letter To The New York Post", "Public Enemy", 1991));
-		songs.add(new Song("Murder Mystery", "Edan", 2005));
-		songs.add(new Song("Heaven's Blade", "Coil", 2005));
-		songs.add(new Song("Daddy's Gonna Tell You No Lie", "Sun Ra", 2005));
-		songs.add(new Song("Burning", "Fugazi", 1989));
-		songs.add(new Song("La goualante de pauvre jean", "Edith Piaf", 2007));
-		songs.add(new Song("Traveling Riverside Blues", "Led Zeppelin", 1982));
-		songs.add(new Song("Sequent C'", "Tangerine Dream", 1974));
-		songs.add(new Song("Mothers Of The Disappeared", "U2", 1987));
-		songs.add(new Song("Down to the Well", "Pixies", 1990));
-		songs.add(new Song("Seras-tu là?", "Michel Berger", 1994));
-		songs.add(new Song("Another Brick In The Wall (Part I)", "Pink Floyd", 1979));
-		songs.add(new Song("She Lives On A Mountain", "Gorky's Zygotic Mynci", 1999));
-		songs.add(new Song("Moody Dipper", "My Education", 2006));
-		return songs;
-	}
-	
-	public static void main(String[] args) {
-		ArrayList<Song> songs = initSongList();
-		MyHashTable<String,Song> songTable;
-		int numBuckets = 7;
-		ArrayList<String> errorLog = new ArrayList<>();
-		
+    /**
+     * Songs to be added to the Table
+     */
+    private static final List<Song> songs = Arrays.asList(
+            new Song("Paranoid Android", "Radiohead", 1997),
+            new Song("Machine Gun", "Slowdive", 1993),
+            new Song("A Change Is Gonna Come", "Sam Cooke", 1964),
+            new Song("15 Step", "Radiohead", 2008),
+            new Song("Blue Line Swinger", "Yo La Tengo", 1995),
+            new Song("Reckoner", "Radiohead", 2008),
+            new Song("Ashes To Ashes", "David Bowie", 1980),
+            new Song("Speed Law", "Mos Def", 1999),
+            new Song("93 'Til Infinity", "Souls of Mischief", 1993),
+            new Song("Season of the Shark", "Yo La Tengo", 2003),
+            new Song("Cet Air-La", "France Gall", 1966),
+            new Song("Space Oddity", "David Bowie", 1969),
+            new Song("Il Nous Faut Regarder", "Jacques Brel", 1955),
+            new Song("Happy Holidays", "Jim O'Rourke", 1999),
+            new Song("Le Premier Bonheur du Jour", "Os Mutantes", 1968),
+            new Song("Stretch Out And Wait", "The Smiths", 1987),
+            new Song("Scream", "Black Flag", 1984),
+            new Song("Europe, After the Rain", "Max Richter", 2002),
+            new Song("Why Are You Looking Grave?", "Mew", 2005),
+            new Song("Fallen Angel", "King Crimson", 1974),
+            new Song("Milk and Honey", "Nick Drake", 2007),
+            new Song("One Less Bell To Answer", "Burt Bacharach", 2003),
+            new Song("A Letter To The New York Post", "Public Enemy", 1991),
+            new Song("Murder Mystery", "Edan", 2005),
+            new Song("Heaven's Blade", "Coil", 2005),
+            new Song("Daddy's Gonna Tell You No Lie", "Sun Ra", 2005),
+            new Song("Burning", "Fugazi", 1989),
+            new Song("La goualante de pauvre jean", "Edith Piaf", 2007),
+            new Song("Traveling Riverside Blues", "Led Zeppelin", 1982),
+            new Song("Sequent C'", "Tangerine Dream", 1974),
+            new Song("Mothers Of The Disappeared", "U2", 1987),
+            new Song("Down to the Well", "Pixies", 1990),
+            new Song("Seras-tu là?", "Michel Berger", 1994),
+            new Song("Another Brick In The Wall (Part I)", "Pink Floyd", 1979),
+            new Song("She Lives On A Mountain", "Gorky's Zygotic Mynci", 1999),
+            new Song("Moody Dipper", "My Education", 2006));
+
+    private static HashMap<String, Song> map;
+    private static MyHashTable<String, Song> songTable;
+    private static ArrayList<String> errorLog = new ArrayList<>();
+
+    public static void main(String[] args) {
+        int numBuckets = 7;
+
 		/* Test 1:
-		 *     - this is just to see whether the hash table works
+         *     - this is just to see whether the hash table works
 		 *     - hash table capacity = 7
 		 *     - compression function = division
 		 */
-		
-		// Initialize the hash table.   Key will be the song title.
-		
-		songTable = new MyHashTable<String,Song>(numBuckets);
-		for (Song song: songs) {
-			songTable.put(song.getTitle(), song);
-		}
 
-		int size1 = songTable.size();
-		int nB1 = songTable.getNumBuckets();
-		System.out.println("number of songs: " + size1);
-		System.out.println("number of buckets in hashtable: " + nB1);
-				
-		//  rehashing changes the capacity of the table, but not the number of entries
-		songTable.rehash();
-		int size2 = songTable.size();
-		int nB2 = songTable.getNumBuckets();
-		System.out.println("number of songs: " + size2);
-		System.out.println("number of buckets in hashtable: " + nB2);
-				
-		// Try to retrieve a song
-		StringBuffer errors = new StringBuffer();
-		Song testSong1 = songTable.get("Scream");
-		if (testSong1 == null || !testSong1.getArtist().equals("Black Flag") || testSong1.getYear() != 1984) {
-			errors.append("Failed to retrieve song 'Scream'.\n");
-		}			
-		
-		// Try to retrieve another song
-		Song testSong2 = songTable.get("Fallen Angel");
-		if (testSong2 == null || !testSong2.getArtist().equals("King Crimson") || testSong2.getYear() != 1974) {
-			errors.append("Failed to retrieve song 'Fallen Angel'.\n");
-		}
-		
-		// Try to remove a song
-		Song removedSong = songTable.remove("Fallen Angel");
-		Song retrievedSong = songTable.get("Fallen Angel");
-		if (removedSong == null || !removedSong.getArtist().equals("King Crimson")
-				|| removedSong.getYear() != 1974 || retrievedSong != null) {
-			errors.append("Failed to remove song 'Fallen Angel'.\n");
-		}
-		
-		//   PUT MORE TESTS HERE.
-				
-		// Display the test results
-		System.out.println("---------------\nTEST 1 RESULTS:\n---------------\n");
-		if (!errorLog.isEmpty()) {
-			for(String s : errorLog) System.out.println(s);
-		} else if (errors.length() == 0) {
-			errors.append("All tests passed successfully.");
-		}
-		
-		System.out.println(errors.toString());
-		System.out.println();
-		
-		// Testing iterator   (NOT NECESSARY IF THEY DON'T IMPLEMENT ITERATOR
-		
-		MyHashTable<String,Song>.HashIterator iter= songTable.iterator();
-		while(iter.hasNext())
-		{
-			Song song=iter.next().getValue();
-			System.out.print(song);
-			System.out.println();
-		}
-		
-		// test containsKey(),  keys(),  values()
-		
-		System.out.println("should be false: " + songTable.containsKey("Purple Haze"));
-		System.out.println("should be true: " + songTable.containsKey("Down to the Well"));
-		System.out.print("SONG TITLES: ");
-		for (String songTitle : songTable.keys())
-			System.out.print(songTitle + ",");
-		System.out.println();
-		System.out.print("ALL SONGS: ");
-		for (Song song:songTable.values())
-			System.out.print(song + ",");
-		
-	}
-	
+        // Initialize the hash table.   Key will be the song title.
+
+        songTable = new MyHashTable<String, Song>(numBuckets);
+        map = new HashMap<>(numBuckets);
+        for (Song song : songs) {
+            songTable.put(song.getTitle(), song);
+            map.put(song.getTitle(), song);
+        }
+
+        int size1 = songTable.size();
+        int nB1 = songTable.getNumBuckets();
+        print("number of songs: " + size1);
+        print("number of buckets in hashtable: " + nB1);
+
+        //  rehashing changes the capacity of the table, but not the number of entries
+        songTable.rehash();
+        int size2 = songTable.size();
+        int nB2 = songTable.getNumBuckets();
+        print("number of songs: " + size2);
+        print("number of buckets in hashtable: " + nB2);
+
+        int sizeTrue = map.size();
+        if (size1 != sizeTrue || size2 != sizeTrue) errorLog.add(s("Bad song size; both sizes should be %d", sizeTrue));
+        if (nB2 / nB1 != 2)
+            errorLog.add(s("Bucket size should be two times greater after rehashing; currently %d and %d", nB1, nB2));
+        if (nB1 % numBuckets != 0)
+            errorLog.add(s("Check your bucket size creation; should have been divisible by %d", numBuckets));
+
+        testGet("Scream");
+        testGet("Fallen Angel");
+        testRemove("Fallen Angel");
+
+        MyHashTable<String, Song>.HashIterator iter = songTable.iterator();
+        int i = 0;
+        while (iter.hasNext()) {
+            i++;
+            Song song = iter.next().getValue();
+            print(song.toString());
+        }
+        if (i != map.size())
+            errorLog.add(s("Iterator did not seem to go through all the keys; found %d of %d", i, map.size()));
+
+        // test containsKey(),  keys(),  values()
+        if (songTable.containsKey("Purple Haze"))
+            errorLog.add("Table should not contain the key 'Purple Haze', but tests show it does");
+        if (!songTable.containsKey("Down to the Well"))
+            errorLog.add("Table should contain the key 'Purple Haze', but tests show it does not");
+
+        System.out.print("SONG TITLES: ");
+        for (String songTitle : songTable.keys())
+            System.out.print(songTitle + ",");
+        print("\n");
+        System.out.print("ALL SONGS: ");
+        for (Song song : songTable.values())
+            System.out.print(song + ",");
+        print("\n");
+
+        // Display the test results
+        print("---------------\nTEST RESULTS:\n---------------\n");
+        for (String s : songTable.keys()) {
+            map.remove(s);
+        }
+        for (String s : map.keySet()) {
+            print(s);
+        }
+        if (!errorLog.isEmpty()) {
+            for (String s : errorLog) print(s);
+        } else {
+            print("All tests passed successfully.");
+        }
+    }
+
+    private static boolean compare(Song s1, Song s2) {
+        if (s1 == null) return s2 == null;
+        if (s2 == null) return false; //s1 is not null but s2 is
+        if (!s1.getTitle().equals(s2.getTitle())) return false;
+        if (!s1.getArtist().equals(s2.getArtist())) return false;
+        return s1.getYear() == s2.getYear();
+    }
+
+    private static void testGet(String title) {
+        if (!compare(songTable.get(title), map.get(title))) errorLog.add(s("Failed to retrieve song '%s'", title));
+    }
+
+    private static void testRemove(String title) {
+        if (!compare(songTable.remove(title), map.remove(title))) {
+            errorLog.add(s("Failed to remove song '%s'", title));
+        } else {
+            testGet(title);
+        }
+    }
+
+    private static String s(String s, Object... o) {
+        return String.format(Locale.CANADA, s, o);
+    }
+
+    private static void print(String s, Object... o) {
+        System.out.println(s(s, o));
+    }
 }
